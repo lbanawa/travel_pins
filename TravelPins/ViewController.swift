02e99e09f19b52cf68ac7529319e71c4aed87e5f
lbanawa <61660817+lbanawa@users.noreply.github.com>
2020-04-05 19:10:33 -0400

    user location feature
    Core Location import, location manager function

--- TravelPins/ViewController.swift
+++ TravelPins/ViewController.swift
@@ -7,16 +7,31 @@
 //
 
 import UIKit
-import MapKit
+import MapKit // allows map function
+import CoreLocation // enables ability to work with user location
 
-class ViewController: UIViewController, MKMapViewDelegate {
+class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate {
     
     @IBOutlet weak var mapView: MKMapView!
+    var locationManager = CLLocationManager() // object used to stop and start delivery of location-related events to app
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         mapView.delegate = self
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest // choose the desired level of accuracy for the location services
+        locationManager.requestWhenInUseAuthorization() // request permission from the user to use location when using the app
+        locationManager.startUpdatingLocation() // get the user location and give it to us
+        
+    }
+    
+    // this is where you can get the current location information
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let location = CLLocationCoordinate2D(latitude: locations[0].coordinate.latitude, longitude: locations[0].coordinate.longitude) // create a location with latitude and longitude
+        let span = MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1) // choose how much you want to zoom in on the map; the smaller the number, the more zoomed in
+        let region = MKCoordinateRegion(center: location, span: span) // the area you'd like shown/ centered on your map
+        mapView.setRegion(region, animated: true)
         
     }
 
