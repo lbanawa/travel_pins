557f2836e90feb8b4c67af581f94094af254b5a9
lbanawa <61660817+lbanawa@users.noreply.github.com>
2020-04-06 10:47:48 -0400

    navigation to pin feature
    allow user to use Apple Maps to navigate to selected pin from current location

--- TravelPins/ViewController.swift
+++ TravelPins/ViewController.swift
@@ -168,6 +168,31 @@ class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDele
         return pinView
     }
     
+    // launch navigation
+    func mapView(_ mapView: MKMapView, annotationView view: MKAnnotationView, calloutAccessoryControlTapped control: UIControl) {
+        if selectedTitle != "" {
+            
+            let requestLocation = CLLocation(latitude: annotationLatitude, longitude: annotationLongitude)
+            
+            CLGeocoder().reverseGeocodeLocation(requestLocation) { (placemarks, error) in
+                // closure
+                if let placemark = placemarks {
+                    if placemark.count > 0 {
+                        
+                        let newPlacemark = MKPlacemark(placemark: placemark[0])
+                        let item = MKMapItem(placemark: newPlacemark)
+                        item.name = self.annotationTitle
+                        let launchOptions = [MKLaunchOptionsDirectionsModeKey:MKLaunchOptionsDirectionsModeDriving] // default mode of transportation is driving
+                        item.openInMaps(launchOptions: launchOptions) // open the navigation -- aka Apple Maps
+                        
+                    }
+                }
+                
+            }
+            
+        }
+    }
+    
     
     
     
