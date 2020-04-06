9f227359bb12fe88d8d5091776c5a8db3743f254
lbanawa <61660817+lbanawa@users.noreply.github.com>
2020-04-05 20:50:00 -0400

    user interface

---
+++ TravelPins/ListViewController.swift
@@ -0,0 +1,41 @@
+//
+//  ListViewController.swift
+//  TravelPins
+//
+//  Created by Lauren Banawa on 4/5/20.
+//  Copyright © 2020 Lauren Banawa. All rights reserved.
+//
+
+import UIKit
+
+class ListViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // create add button in navigation bar
+        navigationController?.navigationBar.topItem?.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: UIBarButtonItem.SystemItem.add, target: self, action: #selector(addButtonClicked))
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+    }
+    
+    @objc func addButtonClicked() {
+        performSegue(withIdentifier: "toViewController", sender: nil)
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = "test"
+        return cell
+    }
+    
+
+
+}
